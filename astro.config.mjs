ca9c14f9478bda2d8ac7c7c7b027c8801f6d1662
Andrii Avramenko <andreitvmm228@gmail.com>
2026-06-19 00:48:38 +0100

    Proper deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://andrii-avramenko.github.io',
     base: '/astroProject/'
 });
